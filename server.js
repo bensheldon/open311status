a52f7312964280e78f96fb326561a2c18b15b965
Ben Sheldon <bensheldon@gmail.com>
2012-05-23 18:00:08 -0700

    Converted Mongoose docs to real objects (TODO: Make this not suck!)

--- server.js
+++ server.js
@@ -63,12 +63,22 @@ app.get('/', function(req, res) {
               .limit(endpointCount)
               .sort('requestedAt', -1)
               .run(function(err, servicesPings) {
+                
       
     RequestsPing.find()
                 .limit(endpointCount)
                 .sort('requestedAt', -1)
                 .run(function(err, requestsPings) {
-    
+       
+       
+      // TODO: Find a better way to convert Mongoose docs to objects
+      servicesPings = servicesPings.map(function (servicesPing) {
+         return servicesPing.toObject();
+       });
+       requestsPings = requestsPings.map(function (requestsPing) {
+         return requestsPing.toObject();
+       });
+          
       res.render('index', { 
           title: 'Open311 Status'
         , endpoints: Endpoints
