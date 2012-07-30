f20923825dfb75e23522c2e13053c314752294e9
Ben Sheldon <bensheldon@gmail.com>
2012-07-30 15:37:29 -0700

    Added a WILLPING environment variable for development to access the production database without writing to it.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express         = require('express')
   , io              = require('socket.io').listen(app);
   
 var PORT = process.env.PORT || 3000;
+var WILLPING = (process.env.WILLPING == "TRUE") ? true : false; // Don't ping by default
 
 var EVERY5MINUTES = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 
@@ -30,41 +31,44 @@ var Pinger = require('./lib/pinger');
 var Endpoints = require('./lib/endpoints');
 var pinger = new Pinger(Endpoints);
 
-/** Set up our Cron **/
-var CronJob = require('cron').CronJob;
 
-// schedule every 5 minutes
-var scheduledPings = new CronJob('00 */5 * * * *', 
-  function() { 
-    // ping all our endpoints
-    pinger.pingAll(function() {
-      // aggregate and save that endpoint data
-      Endpoint.generateAll(function(err, endpoints) {
-        // push those updated+aggregated endpoints to the client
-        io.sockets.emit('endpoints', endpoints);
-      });
-    }); 
-  }, null, true);
+if (WILLPING) {
+  /** Set up our Cron **/
+  var CronJob = require('cron').CronJob;
 
-// Every hour delete pings older than 48 hours old
-var cleanupPings = new CronJob('00 00 * * * *', function() { pinger.cleanUp() }, null, true);
-
-var replayRequests = new CronJob('00 */5 * * * *', function() {
-  var d = new Date();
-  // get all service requests from between 24 hours and 23:55 hours:minutes ago
-  var serviceRequests = ServiceRequest.find()
-                                      .where('requested_datetime').gte(new Date(d.getTime() - 24*60*60*1000)).lte(new Date(d.getTime() - (23*60+55)*60*1000))
-                                      .sort('requested_datetime', 1)
-                                      .stream();
-  serviceRequests.on('data', function (serviceRequest) {
-    serviceRequest = serviceRequest.toObject();
-
-    var when = new Date(serviceRequest.requested_datetime.getTime() + 24*60*60*1000); // add 24 hours
-    var scheduleEmit = new CronJob(when, function(){
-      io.sockets.emit('serviceRequest', serviceRequest); // emit to socket
+  // schedule every 5 minutes
+  var scheduledPings = new CronJob('00 */5 * * * *', 
+    function() { 
+      // ping all our endpoints
+      pinger.pingAll(function() {
+        // aggregate and save that endpoint data
+        Endpoint.generateAll(function(err, endpoints) {
+          // push those updated+aggregated endpoints to the client
+          io.sockets.emit('endpoints', endpoints);
+        });
+      }); 
     }, null, true);
-  });
-}, null, true);
+
+  // Every hour delete pings older than 48 hours old
+  var cleanupPings = new CronJob('00 00 * * * *', function() { pinger.cleanUp() }, null, true);
+
+  var replayRequests = new CronJob('00 */5 * * * *', function() {
+    var d = new Date();
+    // get all service requests from between 24 hours and 23:55 hours:minutes ago
+    var serviceRequests = ServiceRequest.find()
+                                        .where('requested_datetime').gte(new Date(d.getTime() - 24*60*60*1000)).lte(new Date(d.getTime() - (23*60+55)*60*1000))
+                                        .sort('requested_datetime', 1)
+                                        .stream();
+    serviceRequests.on('data', function (serviceRequest) {
+      serviceRequest = serviceRequest.toObject();
+
+      var when = new Date(serviceRequest.requested_datetime.getTime() + 24*60*60*1000); // add 24 hours
+      var scheduleEmit = new CronJob(when, function(){
+        io.sockets.emit('serviceRequest', serviceRequest); // emit to socket
+      }, null, true);
+    });
+  }, null, true);
+}
 
 
 // Express Configuration
