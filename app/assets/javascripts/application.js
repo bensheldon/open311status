8d384a8a2d752b76b3db3ad8baa0316ed5e709b4
Ben Sheldon <bensheldon@gmail.com>
2014-07-10 07:41:30 -0700

    add node websocket push server

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require sockjs
 //= require turbolinks
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Open311status
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.paths << "#{Rails}/vendor/assets/**/*"
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
