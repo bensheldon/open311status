9a5c2e05e6b2f6872769cc970916c6223268c64e
Ben Sheldon <bensheldon@gmail.com>
2018-08-04 18:11:11 -0700

    Add Surrey, BC; and Alexandria, VA (#37)

---
+++ app/models/cities/alexandria.rb
@@ -0,0 +1,8 @@
+module Cities
+  class Alexandria < City
+    configure do |c|
+      c.name = 'Alexandria, VA'
+      c.endpoint = ' https://request.alexandriava.gov/GeoReport/Service.ashx/'
+    end
+  end
+end
