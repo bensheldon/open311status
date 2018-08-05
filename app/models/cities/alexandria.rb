d86a2469f5050cd1c4b5d5a5851ee782080fd657
Ben Sheldon <bensheldon@gmail.com>
2018-08-04 19:37:10 -0700

    Remove space from Alexandria endpoint url

--- app/models/cities/alexandria.rb
+++ app/models/cities/alexandria.rb
@@ -2,7 +2,7 @@ module Cities
   class Alexandria < City
     configure do |c|
       c.name = 'Alexandria, VA'
-      c.endpoint = ' https://request.alexandriava.gov/GeoReport/Service.ashx/'
+      c.endpoint = 'https://request.alexandriava.gov/GeoReport/Service.ashx/'
     end
   end
 end
