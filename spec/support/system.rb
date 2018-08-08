fdf5dc00c0bbd16773715cace992cfb6a9625818
Ben Sheldon <bensheldon@gmail.com>
2018-08-07 22:10:59 -0700

    Add chromedriver system tests (#50)

---
+++ spec/support/system.rb
@@ -0,0 +1,11 @@
+Capybara.server = :puma, { Silent: true }
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 900]
+  end
+end
