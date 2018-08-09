72b5881550ece26e4d3c9bdeedffb0fdc30f14b7
Ben Sheldon <bensheldon@gmail.com>
2018-08-08 17:27:36 -0700

    Update to Rails 5.2 configuration files

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,27 +8,14 @@ Bundler.require(*Rails.groups)
 
 module Open311status
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
-    config.assets.paths << "#{Rails.root}/vendor/assets/**/*"
-
-    custom_paths = %W(
-      #{Rails.root}/lib
-      #{Rails.root}/lib/**
-      #{Rails.root}/app/decorators/collections
-      #{Rails.root}/app/cities
-    )
-    config.autoload_paths += custom_paths
-    config.eager_load_paths += custom_paths
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.paths.add 'app/decorators/collections', eager_load: true
   end
 end
