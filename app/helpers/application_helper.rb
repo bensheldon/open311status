5e2033938f2ac7a43342616cf813d0f1e7f9781c
Ben Sheldon <bensheldon@gmail.com>
2018-08-14 07:37:10 -0700

    Guard against incomplete media urls when upgrading them to https (#67)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
+  def valid_url?(url)
+    return false unless url.respond_to?(:match?)
+    url.match? URI.regexp(%w[http https])
+  end
+
   def force_https(url)
+    return url unless valid_url?(url)
     uri = URI.parse(url)
     uri.scheme = 'https'
     uri.to_s
