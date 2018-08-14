449f1e6c2bf8f52d8e433c2c4febfa6b4451fdad
Ben Sheldon <bensheldon@gmail.com>
2018-08-14 07:33:02 -0700

    Guard against incomplete media urls when upgrading them to https

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,5 +6,27 @@ RSpec.describe ApplicationHelper, type: :helper do
       url = "http://example.com/something.png?key=value"
       expect(helper.force_https(url)).to eq "https://example.com/something.png?key=value"
     end
+    
+    it 'does not upgrade incomplete urls' do
+      url = '/just/a/path'
+      expect(helper.force_https(url)).to eq url
+    end
+  end
+
+  describe 'valid_url?' do
+    it 'returns false if just a path' do
+      url = "http://example.com/something.png?key=value"
+      expect(helper.valid_url?(url)).to eq true
+    end
+
+    it 'returns true if a full url' do
+      url = '/just/a/path'
+      expect(helper.valid_url?(url)).to eq false
+    end
+
+    it 'returns false if not a string' do
+      url = nil
+      expect(helper.valid_url?(url)).to eq false
+    end
   end
 end
