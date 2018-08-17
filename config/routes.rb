e8e0c1149e301f3ff4ef863f6155854b3d3ce48e
Ben Sheldon <bensheldon@gmail.com>
2018-08-17 08:57:04 -0700

    Fix generation of sitemap routes
    Was generating links that look like ``https://status.open311.org/https://status.open311.org/cities/ottawa/requests/201800866923/recreation-and-culture-park-maintenance-garbage-cans-in-parks-full`

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,21 @@ Rails.application.routes.draw do
   get '/sitemap.xml.gz', to: redirect("https://#{Rails.application.secrets.s3_bucket_name}.s3.amazonaws.com/sitemaps/sitemap.xml.gz")
 
   resolve "ServiceRequest" do |service_request, options|
+    service_request.parameterize.merge(controller: 'cities/requests', action: 'show').merge(options)
+  end
+
+  direct :github do
+    'https://github.com/codeforamerica/open311status'
+  end
+end
+
+Rails.application.routes.named_routes.url_helpers_module.module_eval do
+  def service_request_path(service_request, options = {})
+    slug_city_request_path(service_request.city.slug, service_request.service_request_id, service_request.slug, options)
+  end
+
+  def service_request_url(service_request, options = {})
     slug_city_request_url(service_request.city.slug, service_request.service_request_id, service_request.slug, options)
   end
 end
+
