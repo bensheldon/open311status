37cfd23925d3f26a8fbb9f1a62f14940dd8920ef
Ben Sheldon <bensheldon@gmail.com>
2018-08-20 09:41:03 -0700

    Ensure postgis extension is enabled in migration

--- db/migrate/20180819143420_add_geometry_to_service_requests.rb
+++ db/migrate/20180819143420_add_geometry_to_service_requests.rb
@@ -1,5 +1,7 @@
 class AddGeometryToServiceRequests < ActiveRecord::Migration[5.2]
   def change
+    enable_extension "postgis"
+
     add_column :service_requests, :geometry, :geometry, geographic: true
     add_index :service_requests, :geometry, using: :gist
   end
