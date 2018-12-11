4297c8cebc682acc5cb2dfff989fb793e445c4ca
Ben Sheldon <bensheldon@gmail.com>
2018-12-11 07:45:15 -0800

    Eagerload cities in Requests#index controller action

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,6 +2,6 @@ class RequestsController < ApplicationController
   include Pagy::Backend
 
   def index
-    @pagy, @service_requests = pagy(ServiceRequest.order(requested_datetime: :desc))
+    @pagy, @service_requests = pagy(ServiceRequest.includes(:city).order(requested_datetime: :desc))
   end
 end
