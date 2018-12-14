dd41a4c3baab228698b75f92c0bfbf27ee40cac5
Ben Sheldon <bensheldon@gmail.com>
2018-12-13 19:27:48 -0800

    Update FactoryBot factories

--- spec/factories/statuses.rb
+++ spec/factories/statuses.rb
@@ -3,16 +3,16 @@
 FactoryBot.define do
   factory :status do
     city
-    request_name "service_requests"
+    request_name { "service_requests" }
     duration_ms { Random.rand(2000) }
-    http_code 200
+    http_code { 200 }
 
     trait :services do
-      request_name 'service_list'
+      request_name { 'service_list' }
     end
 
     trait :requests do
-      request_name 'service_requests'
+      request_name { 'service_requests' }
     end
   end
 end
