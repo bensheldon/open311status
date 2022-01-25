55621067ed320c4d0ee2452d1d298266a69b4067
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-01-24 19:00:49 -0800

    Return 404 instead of 500 when showing invalid Service Request ID (#160)

--- spec/controllers/cities/requests_controller_spec.rb
+++ spec/controllers/cities/requests_controller_spec.rb
@@ -16,5 +16,15 @@ RSpec.describe Cities::RequestsController, type: :controller do
       get :show, params: { city_slug: service_request.city.slug, service_request_id: service_request.service_request_id, slug: 'not-the-slug' }
       expect(response).to have_http_status(:moved_permanently)
     end
+
+    it 'returns a 404 on invalid cities and ids' do
+      expect do
+        get :show, params: { city_slug: "nope", service_request_id: service_request.service_request_id }
+      end.to raise_error ActiveRecord::RecordNotFound
+
+      expect do
+        get :show, params: { city_slug: service_request.city.slug, service_request_id: "nope" }
+      end.to raise_error ActiveRecord::RecordNotFound
+    end
   end
 end
