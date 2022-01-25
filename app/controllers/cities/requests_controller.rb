0eb8cf99c1e667ab2247fc0d6ef98bb29e690f12
Ben Sheldon <bensheldon@gmail.com>
2022-01-24 18:59:06 -0800

    Return 404 instead of 500 when showing invalid Service Request ID

--- app/controllers/cities/requests_controller.rb
+++ app/controllers/cities/requests_controller.rb
@@ -3,7 +3,7 @@
 module Cities
   class RequestsController < ApplicationController
     def show
-      @service_request = City.find_by!(slug: params[:city_slug]).service_requests.find_by(service_request_id: params[:service_request_id])
+      @service_request = City.find_by!(slug: params[:city_slug]).service_requests.find_by!(service_request_id: params[:service_request_id])
       return redirect_to polymorphic_path(@service_request), status: :moved_permanently if @service_request.slug.present? && params[:slug] != @service_request.slug
     end
   end
