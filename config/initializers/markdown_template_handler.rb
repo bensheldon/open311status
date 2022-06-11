c8ddb95de53c3174b1087f5105655b61dc47f0ef
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-06-10 20:18:12 -0700

    Update markdown template handler

--- config/initializers/markdown_template_handler.rb
+++ config/initializers/markdown_template_handler.rb
@@ -1,13 +1,14 @@
-module MarkdownTemplateHandler
+# frozen_string_literal: true
+module MarkdownHandler
   def self.erb
-    @erb ||= ActionView::Template.registered_template_handler(:erb)
+    @_erb ||= ActionView::Template.registered_template_handler(:erb)
   end
 
-  def self.call(template)
-    compiled_source = erb.call(template)
+  def self.call(template, source)
+    compiled_source = erb.call(template, source)
     "Kramdown::Document.new(begin;#{compiled_source};end, auto_ids: false).to_html.html_safe"
   end
 end
 
-ActionView::Template.register_template_handler :md, MarkdownTemplateHandler
-ActionView::Template.register_template_handler :markdown, MarkdownTemplateHandler
+ActionView::Template.register_template_handler :md, MarkdownHandler
+ActionView::Template.register_template_handler :markdown, MarkdownHandler
