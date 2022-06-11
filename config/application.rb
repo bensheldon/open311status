e5a6e7b19c2c65a09050beb192c755f929f3257a
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-06-10 19:29:20 -0700

    Add good_job

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Open311status
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.active_job.queue_adapter = :good_job
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,19 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_11_155020) do
+ActiveRecord::Schema.define(version: 2022_06_11_013909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "postgis"
 
@@ -23,6 +24,37 @@ ActiveRecord::Schema.define(version: 2018_12_11_155020) do
     t.datetime "updated_at"
     t.integer "service_definitions_count", default: 0, null: false
     t.index ["slug"], name: "index_cities_on_slug", unique: true
+  end
+
+  create_table "good_job_processes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.jsonb "state"
+  end
+
+  create_table "good_jobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.text "queue_name"
+    t.integer "priority"
+    t.jsonb "serialized_params"
+    t.datetime "scheduled_at"
+    t.datetime "performed_at"
+    t.datetime "finished_at"
+    t.text "error"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.uuid "active_job_id"
+    t.text "concurrency_key"
+    t.text "cron_key"
+    t.uuid "retried_good_job_id"
+    t.datetime "cron_at"
+    t.index ["active_job_id", "created_at"], name: "index_good_jobs_on_active_job_id_and_created_at"
+    t.index ["active_job_id"], name: "index_good_jobs_on_active_job_id"
+    t.index ["concurrency_key"], name: "index_good_jobs_on_concurrency_key_when_unfinished", where: "(finished_at IS NULL)"
+    t.index ["cron_key", "created_at"], name: "index_good_jobs_on_cron_key_and_created_at"
+    t.index ["cron_key", "cron_at"], name: "index_good_jobs_on_cron_key_and_cron_at", unique: true
+    t.index ["finished_at"], name: "index_good_jobs_jobs_on_finished_at", where: "((retried_good_job_id IS NULL) AND (finished_at IS NOT NULL))"
+    t.index ["queue_name", "scheduled_at"], name: "index_good_jobs_on_queue_name_and_scheduled_at", where: "(finished_at IS NULL)"
+    t.index ["scheduled_at"], name: "index_good_jobs_on_scheduled_at", where: "(finished_at IS NULL)"
   end
 
   create_table "que_jobs", primary_key: ["queue", "priority", "run_at", "job_id"], comment: "3", force: :cascade do |t|
@@ -75,7 +107,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_155020) do
   end
 
 
-  create_view "global_indices", materialized: true,  sql_definition: <<-SQL
+  create_view "global_indices", materialized: true, sql_definition: <<-SQL
       WITH service_request_indices AS (
            SELECT ('ServiceRequest-'::text || service_requests.id) AS id,
               'ServiceRequest'::text AS searchable_type,
@@ -90,7 +122,6 @@ ActiveRecord::Schema.define(version: 2018_12_11_155020) do
       service_request_indices.content
      FROM service_request_indices;
   SQL
-
   add_index "global_indices", "to_tsvector('english'::regconfig, content)", name: "index_global_indices_on_to_tsvector_english_content", using: :gin
   add_index "global_indices", ["content"], name: "index_global_indices_on_content_gist_trgm_ops", opclass: :gist_trgm_ops, using: :gist
   add_index "global_indices", ["id"], name: "index_global_indices_on_id", unique: true
