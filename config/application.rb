e6798b85dd7ed621b7a9902fd472abc6e7467632
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-09-04 17:35:46 -0700

    Create Fly.io configuration (#182)
    * Create Fly.io configuration
    
    * Upgrade GoodJob
    
    * Update toml

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module Open311status
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.active_job.queue_adapter = :good_job
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_11_150832) do
+ActiveRecord::Schema.define(version: 2022_09_05_003011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
