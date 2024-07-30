e246273f21465584e30cc689d18682fd8a1a039d
Ben Sheldon [he/him] <bensheldon@gmail.com>
2024-07-30 16:40:21 -0700

    Replace sentry-raven with sentry-rails (#270)

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,18 @@
-Raven.configure do |config|
-  config.environments = %w[ production ]
-end
+# frozen_string_literal: true
 
-Raven.tags_context({
-  'environment' => Rails.env
-})
+if defined? Sentry
+  Sentry.init do |config|
+    config.enabled_environments = %w[production staging]
+    config.excluded_exceptions = Sentry::Configuration::IGNORE_DEFAULT + Sentry::Rails::IGNORE_DEFAULT + %w[
+      ActionController::UnknownFormat
+      Rack::Timeout::RequestTimeoutError
+      Rack::Timeout::RequestExpiryError
+    ]
+
+    # use Rails' parameter filter to sanitize the event
+    filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters - [:name, :filename])
+    config.before_send = lambda do |event, _hint|
+      filter.filter(event.to_hash)
+    end
+  end
+end
